d5835e6cf4f667e92db6eacb7703daf66961cdf8
Ashwani Dubey <49708778+ashwanidubey@users.noreply.github.com>
2023-12-04 15:27:09 +0530

    adding project links

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,11 +6,11 @@ export default function Project({ count, setCount }) {
         setCount(4);
     })
     const projects = [{
-        p_name: "iNotebook Cloud", p_type: "Personal Project", p_desc: "User can add notes and save in cloud later he can see notes from any device using his credentials", git: "#", hlink: "#", tech: "TechStack: React, Node , MongoDb , Bootstrap"
+        p_name: "iNotebook Cloud", p_type: "Personal Project", p_desc: "User can add notes and save in cloud later he can see notes from any device using his credentials", git: "https://github.com/ashwanidubey/iNotebook-frontend", hlink: "#", tech: "TechStack: React, Node , MongoDb , Bootstrap"
     }, {
-        p_name: "Flipcart Clone", p_type: "Personal Project", p_desc: "It is Flipcart clone , Purpose of this project is only for learning and show my skills ", git: "#", hlink: "#", tech: "TechStack: React, Node , MongoDb , Bootstrap"
+        p_name: "Flipcart Clone", p_type: "Personal Project", p_desc: "It is Flipcart clone , Purpose of this project is only for learning and show my skills ", git: "https://github.com/ashwanidubey/flipcart-frontend", hlink: "https://flipcart-frontend.vercel.app/", tech: "TechStack: React, Node , MongoDb , Bootstrap"
     }, {
-        p_name: "Student Performace Analysis", p_type: "College Project", p_desc: "I worked as a backend developer on a college project designed to support students in their exam preparations. Our platform conducted tests and used machine learning to provide personalized recommendations based on students' performance. These recommendations helped students improve their study strategies for better results.", git: "#", hlink: "#", tech: "TechStack: Html,CSS, JS, django , ML Libraries (matplolib , sklearn ,linear regression ,pandas)"
+        p_name: "Student Performace Analysis", p_type: "College Project", p_desc: "I worked as a backend developer on a college project designed to support students in their exam preparations. Our platform conducted tests and used machine learning to provide personalized recommendations based on students' performance. These recommendations helped students improve their study strategies for better results.", git: "https://github.com/ashwanidubey/Student-Performance-Analysis/tree/master/student_performance_analysis", hlink: "#", tech: "TechStack: Html,CSS, JS, django , ML Libraries (matplolib , sklearn ,linear regression ,pandas)"
     }
     ];
     return (
